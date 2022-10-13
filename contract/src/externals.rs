b7529d7f711d0e8080f8ec52f61d9cacf16f4ecf
syu <stevenyu530@gmail.com>
2022-10-13 10:04:52 +0100

    update external interface

--- contract/src/externals.rs
+++ contract/src/externals.rs
@@ -7,6 +7,7 @@ use near_sdk::{ext_contract, AccountId};
 // }
 
 // NFT interface, for cross-contract calls
+// For details, refer to NEP-171
 #[ext_contract(ext_nft)]
 pub trait Nft {
     // cross-contract call
@@ -20,9 +21,10 @@ pub trait Nft {
 
     fn nft_transfer_call(
         &mut self,
-        receiver_id: AccountId,
-        token_id: TokenId,
-        memo: Option<String>,
-        msg: String,
+        receiver_id: AccountId,   // account to receive the token
+        token_id: TokenId,        // nft token to be sent
+        approval_id: Option<u64>, // approval ID, in case transfer is sent from ppl with valid approval
+        memo: Option<String>,     
+        msg: String,              // info needed by the receiving contract to handl the transfer.
     );
 }
