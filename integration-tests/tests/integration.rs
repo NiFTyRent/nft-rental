bf079896abd09f671b22ff742b42f56c1ca48c3f
Libo Shen <liboooshen@gmail.com>
2022-12-01 16:24:21 +0000

    Setup integration test framework

--- integration-tests/tests/integration.rs
+++ integration-tests/tests/integration.rs
@@ -1,27 +1,29 @@
-use near_contract_standards::non_fungible_token::{Token};
+use near_contract_standards::non_fungible_token::Token;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_units::{parse_gas, parse_near};
+use near_units::parse_near;
 use serde_json::json;
-use std::{env, fs};
-use tracing::info;
 use workspaces::prelude::*;
 use workspaces::{network::Sandbox, Account, Contract, Worker};
 
-const ONE_MIN_IN_NANO: u64 = 60 * 1000000000;
 const ONE_BLOCK_IN_NANO: u64 = 2000000000;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let wasm_arg: &str = &(env::args().nth(1).unwrap());
-    let nft_wasm_arg: &str = &(env::args().nth(2).unwrap());
-    let wasm_filepath = fs::canonicalize(env::current_dir()?.join(wasm_arg))?;
-    let nft_wasm_filepath = fs::canonicalize(env::current_dir()?.join(nft_wasm_arg))?;
+struct Context {
+    lender: Account,
+    borrower: Account,
+    contract: Contract,
+    nft_contract: Contract,
+    worker: Worker<Sandbox>,
+}
 
+const CONTRACT_CODE: &[u8] =
+    include_bytes!("../../contract/target/wasm32-unknown-unknown/release/nft_rental.wasm");
+const NFT_CODE: &[u8] =
+    include_bytes!("../../demo_nft_contract/target/wasm32-unknown-unknown/release/tamagotchi.wasm");
+
+async fn init() -> anyhow::Result<Context> {
     let worker = workspaces::sandbox().await?;
-    let wasm = std::fs::read(wasm_filepath)?;
-    let nft_wasm = std::fs::read(nft_wasm_filepath)?;
-    let contract = worker.dev_deploy(&wasm).await?;
-    let nft_contract = worker.dev_deploy(&nft_wasm).await?;
+    let contract = worker.dev_deploy(CONTRACT_CODE).await?;
+    let nft_contract = worker.dev_deploy(NFT_CODE).await?;
 
     // create accounts
     let account = worker.dev_create_account().await?;
@@ -57,10 +59,13 @@ async fn main() -> anyhow::Result<()> {
         .transact()
         .await?;
 
-    // begin tests
-    test_create_lease(&alice, &bob, &contract, &nft_contract, &worker).await?;
-    // test_changes_message(&alice, &bob, &contract, &nft_contract, &worker).await?;
-    Ok(())
+    Ok(Context {
+        lender: alice,
+        borrower: bob,
+        contract,
+        nft_contract,
+        worker,
+    })
 }
 
 // TODO(libo): we can import them from the contract under testing.
@@ -84,14 +89,14 @@ struct LeaseCondition {
     amount_near: u128,     // proposed lease cost
     state: LeaseState,     // current lease state
 }
-
-async fn test_create_lease(
-    lender: &Account,
-    borrower: &Account,
-    contract: &Contract,
-    nft_contract: &Contract,
-    worker: &Worker<Sandbox>,
-) -> anyhow::Result<()> {
+#[tokio::test]
+async fn test_create_lease() -> anyhow::Result<()> {
+    let context = init().await?;
+    let lender = context.lender;
+    let borrower = context.borrower;
+    let contract = context.contract;
+    let nft_contract = context.nft_contract;
+    let worker = context.worker;
     let token_id = "test";
     let latest_block = worker.view_latest_block().await?;
     let expiration_ts_nano = latest_block.timestamp() + ONE_BLOCK_IN_NANO * 10;
@@ -166,17 +171,14 @@ async fn test_create_lease(
         .max_gas()
         .transact()
         .await?;
-    
-    let owned_tokens:Vec<Token> = nft_contract
-        .call(&worker,  "nft_tokens_for_owner")
-        .args_json(
-            json!({ 
-                "account_id": lender.id().to_string(), }),
-        )?
+
+    let owned_tokens: Vec<Token> = nft_contract
+        .call(&worker, "nft_tokens_for_owner")
+        .args_json(json!({
+            "account_id": lender.id().to_string(), }))?
         .transact()
         .await?
         .json()?;
-    
 
     let nft_token = &owned_tokens[0];
     assert_eq!(nft_token.token_id, token_id);
@@ -185,77 +187,76 @@ async fn test_create_lease(
     Ok(())
 }
 
-async fn test_changes_message(
-    lender: &Account,
-    borrower: &Account,
-    contract: &Contract,
-    nft_contract: &Contract,
-    worker: &Worker<Sandbox>,
-) -> anyhow::Result<()> {
-    let token_id = "test_accept";
+// async fn test_changes_message(
+//     lender: &Account,
+//     borrower: &Account,
+//     contract: &Contract,
+//     nft_contract: &Contract,
+//     worker: &Worker<Sandbox>,
+// ) -> anyhow::Result<()> {
+//     let token_id = "test_accept";
 
-    // set up
-    lender
-        .call(&worker, nft_contract.id(), "nft_approve")
-        .args_json(json!({
-            "token_id": "test",
-            "account_id": contract.id(),
-            "msg": json!({"contract_addr": nft_contract.id(),
-                          "token_id": token_id,
-                          "borrower": borrower.id(),
-                          "expiration": 3600,
-                          "amount_near": "1"
-            }).to_string()
-        }))?
-        .deposit(parse_near!("1 N"))
-        .transact()
-        .await?;
+//     // set up
+//     lender
+//         .call(&worker, nft_contract.id(), "nft_approve")
+//         .args_json(json!({
+//             "token_id": "test",
+//             "account_id": contract.id(),
+//             "msg": json!({"contract_addr": nft_contract.id(),
+//                           "token_id": token_id,
+//                           "borrower": borrower.id(),
+//                           "expiration": 3600,
+//                           "amount_near": "1"
+//             }).to_string()
+//         }))?
+//         .deposit(parse_near!("1 N"))
+//         .transact()
+//         .await?;
 
-    let leases: Vec<(String, LeaseCondition)> = contract
-        .call(&worker, "leases_by_owner")
-        .args_json(json!({"account_id": lender.id()}))?
-        .transact()
-        .await?
-        .json()?;
+//     let leases: Vec<(String, LeaseCondition)> = contract
+//         .call(&worker, "leases_by_owner")
+//         .args_json(json!({"account_id": lender.id()}))?
+//         .transact()
+//         .await?
+//         .json()?;
 
-    // test, pre-accept: TODO
+//     // test, pre-accept: TODO
 
-    // test, accept occured
-    let lease_id = &leases[0].0;
-    borrower
-        .call(&worker, contract.id(), "lending_accept")
-        .args_json(json!({
-            "lease_id": lease_id,
-        }))?
-        .deposit(1)
-        .transact()
-        .await?;
+//     // test, accept occured
+//     let lease_id = &leases[0].0;
+//     borrower
+//         .call(&worker, contract.id(), "lending_accept")
+//         .args_json(json!({
+//             "lease_id": lease_id,
+//         }))?
+//         .deposit(1)
+//         .transact()
+//         .await?;
 
-    let leases_by_borrower: Vec<(String, LeaseCondition)> = contract
-        .call(&worker, "leases_by_borrower")
-        .args_json(json!({"account_id": borrower.id()}))?
-        .transact()
-        .await?
-        .json()?;
+//     let leases_by_borrower: Vec<(String, LeaseCondition)> = contract
+//         .call(&worker, "leases_by_borrower")
+//         .args_json(json!({"account_id": borrower.id()}))?
+//         .transact()
+//         .await?
+//         .json()?;
 
-    assert_eq!(leases.len(), 1);
+//     assert_eq!(leases.len(), 1);
 
-    let borrower_id_result: String = borrower
-        .call(&worker, contract.id(), "get_borrower")
-        .args_json(json!({
-            "contract_id": nft_contract.id(),
-            "token_id": token_id,
-        }))?
-        .transact()
-        .await?
-        .json()?;
+//     let borrower_id_result: String = borrower
+//         .call(&worker, contract.id(), "get_borrower")
+//         .args_json(json!({
+//             "contract_id": nft_contract.id(),
+//             "token_id": token_id,
+//         }))?
+//         .transact()
+//         .await?
+//         .json()?;
 
-    // test. rent expires
+//     // test. rent expires
 
-    // let lease = &leases_by_borrower[0].1;
+//     // let lease = &leases_by_borrower[0].1;
 
-    println!("{:?}", borrower_id_result);
+//     println!("{:?}", borrower_id_result);
 
-    println!("      Passed ✅ changes message");
-    Ok(())
-}
+//     println!("      Passed ✅ changes message");
+// }
