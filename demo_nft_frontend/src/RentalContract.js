4c9dd7214d6ebebbd9a73a6b3319929a8bfb4136
Libo Shen <liboooshen@gmail.com>
2023-01-04 16:06:57 +0000

    Rename get_borrower

--- demo_nft_frontend/src/RentalContract.js
+++ demo_nft_frontend/src/RentalContract.js
@@ -1,16 +1,15 @@
-const { connect, Contract, keyStores, WalletConnection } = window.nearApi;
-import { nearConfig } from "./near-api";
+const { Contract } = window.nearApi;
 
 // Initialize contract & set global variables
 export async function initContract(contractName) {
   return await new Contract(window.walletConnection.account(), contractName, {
-    viewMethods: ["get_borrower"],
+    viewMethods: ["get_borrower_by_contract_and_token"],
     changeMethods: [],
   });
 }
 
 export async function getBorrower(contract, nftContractId, tokenId) {
-  let res = await contract.get_borrower({
+  let res = await contract.get_borrower_by_contract_and_token({
     contract_id: nftContractId,
     token_id: tokenId,
   });
