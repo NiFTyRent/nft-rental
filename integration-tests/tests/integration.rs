bd320f261fe4e574641fb8fb08f3272e45414d28
Libo Shen <liboooshen@gmail.com>
2023-01-06 01:12:53 +0000

    Update integration test to check royalty split

--- integration-tests/tests/integration.rs
+++ integration-tests/tests/integration.rs
@@ -1,8 +1,15 @@
 use near_contract_standards::non_fungible_token::Token;
-use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{
+    serde::{Deserialize, Serialize},
+    ONE_NEAR,
+};
 use near_units::parse_near;
 use serde_json::json;
 use workspaces::{network::Sandbox, Account, Contract, Worker};
+
+use crate::utils::assert_aprox_eq;
+
+mod utils;
 
 const ONE_BLOCK_IN_NANO: u64 = 2000000000;
 
@@ -101,6 +108,7 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
     let nft_contract = context.nft_contract;
     let worker = context.worker;
     let token_id = "test";
+    let price = ONE_NEAR;
     let latest_block = worker.view_block().await?;
     let expiration_ts_nano = latest_block.timestamp() + ONE_BLOCK_IN_NANO * 10;
 
@@ -114,7 +122,7 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
                           "token_id": token_id,
                           "borrower_id": borrower.id(),
                           "expiration": expiration_ts_nano,
-                          "price": "1"
+                          "price": price.to_string(),
             }).to_string()
         }))
         .deposit(parse_near!("1 N"))
@@ -139,7 +147,7 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
     assert_eq!(lease.lender_id, lender.id().to_string());
     assert_eq!(lease.borrower_id, borrower.id().to_string());
     assert_eq!(lease.expiration, expiration_ts_nano);
-    assert_eq!(lease.price, 1);
+    assert_eq!(lease.price, price);
     assert_eq!(lease.state, LeaseState::Pending);
     println!("      ✅ Lease creation confirmed");
 
@@ -150,7 +158,7 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
         .args_json(json!({
             "lease_id": lease_id,
         }))
-        .deposit(1)
+        .deposit(price)
         .max_gas()
         .transact()
         .await?
@@ -174,6 +182,8 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
     // Fast foward and check expiration
     worker.fast_forward(12).await?;
     println!("Claiming back the NFT...");
+    let lender_balance_before_claim_back = lender.view_account().await?.balance;
+    let nft_contract_balance_before_claim_back = nft_contract.view_account().await?.balance;
     lender
         .call(contract.id(), "claim_back")
         .args_json(json!({
@@ -183,6 +193,21 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
         .transact()
         .await?
         .into_result()?;
+
+    let lender_balance_after_claim_back = lender.view_account().await?.balance;
+    let nft_contract_balance_after_claim_back = nft_contract.view_account().await?.balance;
+    // This is based on the demo NFT royalty logic: the NFT contract always keep 5% for itself.
+    // So the lender get the rest 95% of the rent.
+    assert_aprox_eq(
+        lender_balance_after_claim_back - lender_balance_before_claim_back,
+        price / 20 * 19,
+    );
+
+    assert_aprox_eq(
+        nft_contract_balance_after_claim_back - nft_contract_balance_before_claim_back,
+        price / 20,
+    );
+    println!("      ✅ Royalty splits are correct");
 
     let owned_tokens: Vec<Token> = nft_contract
         .call("nft_tokens_for_owner")
@@ -198,8 +223,8 @@ async fn test_claim_back_success() -> anyhow::Result<()> {
     Ok(())
 }
 
-// Alice creates a lease to Bob. 
-// Bob can accept the lease for the first time 
+// Alice creates a lease to Bob.
+// Bob can accept the lease for the first time
 // but he should fail if he attempts to accept it for multipe times
 #[tokio::test]
 async fn test_accept_leases_already_lent() -> anyhow::Result<()> {
@@ -251,7 +276,7 @@ async fn test_accept_leases_already_lent() -> anyhow::Result<()> {
         .transact()
         .await?
         .into_result()?;
-    
+
     let leases_updated: Vec<(String, LeaseCondition)> = contract
         .call("leases_by_owner")
         .args_json(json!({"account_id": lender.id()}))
@@ -335,7 +360,7 @@ async fn test_accept_lease_fails_already_transferred() -> anyhow::Result<()> {
         .into_result()?;
 
     // Verify the ownership of the token has been updated
-    let token:Token = nft_contract
+    let token: Token = nft_contract
         .view("nft_token")
         .args_json(json!({
             "token_id": token_id,
