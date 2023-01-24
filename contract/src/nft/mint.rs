bf881a1b14e826ffc05919531adc83cd2b2c6f68
Steven Yu <StevenYu530@gmail.com>
2023-01-24 12:25:00 +0000

    support mint nft

--- contract/src/nft/mint.rs
+++ contract/src/nft/mint.rs
@@ -1,6 +1,5 @@
-use near_contract_standards::non_fungible_token::{Token, metadata::TokenMetadata};
-
 use crate::*;
+use near_contract_standards::non_fungible_token::{refund_deposit};
 
 #[near_bindgen]
 impl Contract {
@@ -11,6 +10,31 @@ impl Contract {
         metadata: TokenMetadata,
         receiver_id: AccountId,
     ){
-        todo!()
+        //measure the initial storage being used on the contract
+        let initial_storage_usage = env::storage_usage();
+
+        let token = Token {
+            token_id: token_id,
+            owner_id: receiver_id,
+        };
+
+        //insert the token ID and token struct, when the token doesn't exist
+        assert!(
+            self.tokens_by_id.insert(&token_id, &token).is_none(),
+            "Token already exists"
+        );
+
+        //insert the token ID and metadata
+        self.token_metadata_by_id.insert(&token_id, &metadata);
+
+        //add token_id to its owner - currently, it is infered by lease_ids_by_lender
+        // todo(syu): double check if the record can cause problem before lease become active
+
+        //calculate the required storage
+        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
+
+        //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
+        refund_deposit(required_storage_in_bytes);
+
     }
 }
