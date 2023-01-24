50734e3b2f9137a0d18f6cbf1658505731e95976
Steven Yu <StevenYu530@gmail.com>
2023-01-24 13:59:58 +0000

    add interface for core

--- contract/src/nft/core.rs
+++ contract/src/nft/core.rs
@@ -1,10 +1,92 @@
 use crate::*;
+use near_sdk::PromiseOrValue;
 
-pub trait NonFungibleTokenCore{
+pub trait NonFungibleTokenCore {
+    fn nft_transfer(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>, // NEP-178 Approval
+        memo: Option<String>,
+    );
+
+    //transfers an NFT to a receiver and calls a function on the receiver ID's contract
+    /// Returns `true` if the token was transferred from the sender's account.
+    fn nft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<bool>;
+
+    //get information about the NFT token passed in
+    fn nft_token(&self, token_id: TokenId) -> Option<JsonToken>;
 }
 
-impl Contract {
-    pub fn nft_tokens() {
-        todo!();
+#[ext_contract(ext_nft_receiver)]
+trait NonFungibleTokenReceiver {
+    /*
+        Method on the receiver contract that is called via XCC when nft_transfer_call is called
+        Returns `true` if the token should be returned back to the sender.
+    */
+    fn nft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        previous_owner_id: AccountId,
+        token_id: TokenId,
+        msg: String,
+    ) -> Promise;
+}
+
+#[ext_contract(ext_self)]
+trait NonFungibleTokenResolver {
+    /*
+        This method resolves the promise returned from the XCC to the receiver contract.
+        as part of the nft_transfer_call method
+    */
+    fn nft_resolve_transfer(
+        &mut self,
+        owner_id: AccountId,
+        receiver_id: AccountId,
+        token_id: TokenId,
+    ) -> bool;
+}
+
+impl NonFungibleTokenCore for Contract {
+    fn nft_transfer(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>, // NEP-178 Approval
+        memo: Option<String>,
+    ) {
+        todo!()
+    }
+
+    fn nft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<bool> {
+        todo!()
+    }
+
+    fn nft_token(&self, token_id: TokenId) -> Option<JsonToken> {
+        todo!()
+    }
+}
+
+impl NonFungibleTokenReceiver for Contract {
+    fn nft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        previous_owner_id: AccountId,
+        token_id: TokenId,
+        msg: String,
+    ) -> Promise {
+        todo!()
     }
 }
--- contract/src/nft/mod.rs
+++ contract/src/nft/mod.rs
@@ -1,5 +1,5 @@
 pub mod core;
 pub mod enumeration;
+pub mod internal;
 pub mod metadata;
 pub mod mint;
-pub mod util;
