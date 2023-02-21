e1bf2fc6c202644c5fca0af4a7e62873942d8347
Steven Yu <StevenYu530@gmail.com>
2023-02-21 11:25:52 +0000

    add helper method on lease_id to token_id

--- integration-tests/tests/utils.rs
+++ integration-tests/tests/utils.rs
@@ -1,6 +1,14 @@
+use nft_rental::LeaseId;
+use near_contract_standards::non_fungible_token::TokenId;
+
 /// Helper function in test to check if two large number are close enough (<0.5%).
 pub fn assert_aprox_eq(a: u128, b: u128) {
     assert!(a.abs_diff(b) < (a + b) / 200)
+}
+
+pub fn lease_id_to_lease_token_id(lease_id: &LeaseId) -> TokenId {
+    let suffix: &str = "_lender";
+    format!("{}{}", lease_id, suffix)
 }
 
 /// Assert the equality between two structs which need to be converted into string.
