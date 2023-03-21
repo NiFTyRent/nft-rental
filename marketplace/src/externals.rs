7b0d34fd4c866641cca0f3605bea4a0535d3e194
Steven Yu <StevenYu530@gmail.com>
2023-03-21 16:49:33 +0000

    add required external RPCs

--- marketplace/src/externals.rs
+++ marketplace/src/externals.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use near_sdk::PromiseOrValue;
 
 /// NFT interface, for cross-contract calls
 #[ext_contract(ext_nft)]
@@ -15,11 +16,21 @@ pub trait NonFungibleToken {
 
 #[ext_contract(ext_ft)]
 pub trait FungibleToken {
-    fn ft_transfer_call(
+    fn ft_transfer(
         &mut self,
         receiver_id: AccountId,
         amount: U128,
-        msg: String,
         memo: Option<String>,
     );
+}
+
+/// External interface of this marketplace contract
+#[ext_contract(ext_self)]
+trait ExtSelf {
+    fn transfer_rent_after_nft_transfer(
+        &mut self,
+        ft_contract_id: AccountId,
+        amount: U128,
+        memo: Option<String>,
+    ) -> PromiseOrValue<U128>;
 }
