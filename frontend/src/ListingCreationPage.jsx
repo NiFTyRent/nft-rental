7333c78bc2204f6a25d0d1a00e67f4c87da9e002
Libo Shen <liboooshen@gmail.com>
2023-05-16 16:48:44 +0100

    Fix typo

--- frontend/src/ListingCreationPage.jsx
+++ frontend/src/ListingCreationPage.jsx
@@ -91,7 +91,7 @@ export default function ListingCreationPage() {
       <div className="py-6">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl mb-8 font-semibold text-gray-900">
-            Lent My NFT
+            Lend My NFT
           </h1>
         </div>
 
